0330e54fd4a6cbd81e249f54f4faabe02c511643
Bennet-Ukoh <bennetukoh@gmail.com>
2022-07-01 19:39:37 -0700

    3-print_alphabets.c updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,29 +16,22 @@ int main(void)
 
 
 				while (c <= 'z')
-
-						{
+				{
 
 									putchar(c);
 
 											c++;
-
-												}
+				}
 
 					while (d <= 'Z')
-
-							{
+					{
 
 										putchar(d);
 
 												d++;
-
-													}
+					}
 
 						putchar('\n');
 
 							return (0);
-
-
-
 }
