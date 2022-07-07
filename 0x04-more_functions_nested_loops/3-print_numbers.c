3637f0168f53a59a1a6a3ae5f78d9b05ad8362b1
Bennet-Ukoh <bennetukoh@gmail.com>
2022-07-07 05:43:31 -0700

    betty doc. updated

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,21 @@
  * print_numbers - prints numbers from 0 to 9
  */
 void print_numbers(void)
-{
-	char i;
 
-	for (i = 0; i <= 9; i++)
+{
+
+	char c;
+
+
+
+	for (c = '0'; c <= '9'; c++)
+
 	{
-		_putchar(i);
+
+		_putchar(c);
+
 	}
+
 	_putchar('\n');
+
 }
