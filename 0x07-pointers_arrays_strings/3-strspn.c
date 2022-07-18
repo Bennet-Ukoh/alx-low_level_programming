91567754bfb36a4461f80986e5285c4a5a3e83ad
Bennet-Ukoh <bennetukoh@gmail.com>
2022-07-18 13:31:44 -0700

    betty style errow checked

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,23 @@
+#include "main.h"
 /**
- * _strspn - gets the length of a prefix substring
- * @s: segment to compare bytes from
- * @accept: string of bytes to compare with
- * Return: number of bytes in segment s which consist of bytes from accept
+ * _strspn - gets lengthof a prefix substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: number of bytes of s in accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
-	i = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i]; i++)
 	{
-		j = 0;
-
-		while (accept[j] != '\0' && s[i] != accept[j])
-			j++;
-		if (accept[j] == '\0')
-			return (i);
-		i++;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
 	}
 	return (i);
 }
