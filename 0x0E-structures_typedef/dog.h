aeb5e6040ef05522768430a5da91fb3fb0de8766
Bennet-Ukoh <bennetukoh@gmail.com>
2022-08-02 09:09:06 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - holds information about dogs
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}my_dog;
