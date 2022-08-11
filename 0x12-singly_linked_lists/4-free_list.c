49aba22e998a34bf6a09e25658cd0accd4acfa3d
Bennet-Ukoh <bennetukoh@gmail.com>
2022-08-11 15:18:34 -0700

    error fixed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,16 +9,15 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current;
-	list_t *next;
+        list_t *current;
+        list_t *next;
 
-	current = head;
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current->str);
-		free(current);
-		current = next;
-	}
+        current = head;
+        while (current != NULL)
+        {
+                next = current->next;
+                free(current->str);
+                free(current);
+                current = next;
+        }
 
-}
