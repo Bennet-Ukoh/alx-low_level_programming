e5c022deb942cd2d835bdc8fa8e92994e1c12322
Bennet-Ukoh <bennetukoh@gmail.com>
2022-08-15 17:45:53 -0700

    betty style error fixed

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
- * listint_len - prints number of elements in the list
- * @h: points to the list to be printed
+ * listint_len - prints number of elements in the linked listint_t list
+ * @h: points to the linked list to be printed
  *
- * Return: number of elements
+ * Return: number of elements in the linked listint_t list
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
+
 	while (h != NULL)
 	{
 		count++;
