b6cfc961b5a578993414f4686bcafaaa83ac5721
Bennet-Ukoh <bennetukoh@gmail.com>
2022-10-03 21:00:40 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - add two numbers
+ * @a: num to be added
+ * @b: num to be added
+ *
+ */
+add(a, b)
+{
+    return a + b;
+}
+
+/**
+ * sub - add two numbers
+ * @a: num to be subtracted
+ * @b: num to be subtracted
+ *
+ */
+sub(a, b)
+{
+    return a - b;
+}
+
+/**
+ * mul- add two numbers
+ * @a: num to be multiplied
+ * @b: num to be multiplied
+ *
+ */
+mul(a, b)
+{
+    return a * b;
+}
+
+/**
+ * div - add two numbers
+ * @a: num to be divided
+ * @b: num to be divided
+ *
+ */
+div(a, b)
+{
+    return a / b;
+}
+
+/**
+ * mod - add two numbers
+ * @a: num whose modulus is to be found
+ * @b: num to whose modulus is to be cal
+ *
+ */
+mod(a, b)
+{
+    return a % b;
+}
